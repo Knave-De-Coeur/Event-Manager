df84303551387091878f51c723a9d8c5269b1bba
Knave-De-Coeur <alexanderm.1496@gmail.com>
2023-04-07 15:45:49 +0200

    Added unit tests in composer and 1 small unit Test.php

---
+++ app/src/controllers/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace src\controllers\CityController;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testMyTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
